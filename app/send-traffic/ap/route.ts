bdb819b6c7bdb0d386c87a321108ce719752fd0c
Garrett Tolbert <garrett@vercel.com>
2024-05-28 18:40:58 -0500

    add 2 more regions

---
+++ app/send-traffic/ap/route.ts
@@ -0,0 +1,7 @@
+import { sendTraffic } from '../lib';
+
+export const preferredRegion = ['hnd1', 'bom1'];
+export const runtime = 'edge';
+export const maxDuration = 60;
+
+export const GET = async () => sendTraffic();
