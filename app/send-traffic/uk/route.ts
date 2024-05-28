7d57004c12a2f7a33f51402c5a767af209b934c3
Garrett Tolbert <garrett@vercel.com>
2024-05-28 18:09:50 -0500

    add maxDuration

--- app/send-traffic/uk/route.ts
+++ app/send-traffic/uk/route.ts
@@ -2,5 +2,6 @@ import { sendTraffic } from '../lib';
 
 export const preferredRegion = ['lhr1', 'dub1'];
 export const runtime = 'edge';
+export const maxDuration = 60;
 
 export const GET = async () => sendTraffic();
--- app/send-traffic/us/route.ts
+++ app/send-traffic/us/route.ts
@@ -2,5 +2,6 @@ import { sendTraffic } from '../lib';
 
 export const preferredRegion = ['iad1', 'sfo1'];
 export const runtime = 'edge';
+export const maxDuration = 60;
 
 export const GET = async () => sendTraffic();
