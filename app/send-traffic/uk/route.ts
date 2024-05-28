869a159cd068bfd6365e92f774f775f257f58b9e
Garrett Tolbert <garrett@vercel.com>
2024-05-28 17:23:25 -0500

    make endpoints run at edge

--- app/send-traffic/uk/route.ts
+++ app/send-traffic/uk/route.ts
@@ -1,5 +1,6 @@
 import { sendTraffic } from '../lib';
 
 export const preferredRegion = ['lhr1', 'dub1'];
+export const runtime = 'edge';
 
 export const GET = async () => sendTraffic();
--- app/send-traffic/us/route.ts
+++ app/send-traffic/us/route.ts
@@ -1,5 +1,6 @@
 import { sendTraffic } from '../lib';
 
 export const preferredRegion = ['iad1', 'sfo1'];
+export const runtime = 'edge';
 
 export const GET = async () => sendTraffic();
